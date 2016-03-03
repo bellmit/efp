d46402291f366521b0d6969c6682b08b928a3184
gaokemeng <gaokemeng@baiwang.com>
2016-03-03 20:43:23 +0800

    Signed-off-by: gaokemeng <gaokemeng@baiwang.com>

--- src/main/java/com/baiwang/einvoice/qz/controller/EnumResposeMessageTask.java
+++ src/main/java/com/baiwang/einvoice/qz/controller/EnumResposeMessageTask.java
@@ -2,12 +2,17 @@ package com.baiwang.einvoice.qz.controller;
 
 import java.util.concurrent.Callable;
 
+import javax.annotation.Resource;
 import javax.jms.TextMessage;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+
+import com.baiwang.einvoice.qz.beans.ResultOfKp;
+import com.baiwang.einvoice.qz.service.IResultOfKpService;
+import com.baiwang.einvoice.util.InvoiceUtil;
 
 public class EnumResposeMessageTask implements  Callable<String>{
 	
@@ -16,17 +21,31 @@ public class EnumResposeMessageTask implements  Callable<String>{
 	@Autowired
     private JmsTemplate jmsTemplate2;
 	
-	private String messageSelector;
+	private String ddhm;
+	
+	@Resource
+	private IResultOfKpService resultService;
 	
 	public EnumResposeMessageTask(String xx){
-		this.messageSelector = "JMSCorrelationID = '" + xx +"'";
+		this.ddhm =  xx;
 	}
 	
 	public String call() throws Exception {
 		
-		TextMessage receive = (TextMessage) jmsTemplate2.receiveSelected(messageSelector);
-		logger.info("*****JMSCorrelationID为:" + messageSelector + "接收到的message为：" + receive.getText());
+		TextMessage receive = (TextMessage) jmsTemplate2.receiveSelected("JMSCorrelationID = '" + ddhm +"'");
+		logger.info("*****JMSCorrelationID为:" + ddhm + "接收到的message为：" + receive.getText());
 		
+		ResultOfKp result = new ResultOfKp();
+		result.setCode(InvoiceUtil.getIntervalValue(receive.getText(),"<RETURNCODE>","</RETURNCODE>"));
+		result.setMsg(InvoiceUtil.getIntervalValue(receive.getText(),"<RETURNMSG>","</RETURNMSG>"));
+		result.setFpqqlsh(InvoiceUtil.getIntervalValue(receive.getText(),"<FPQQLSH>","</FPQQLSH>"));
+		try{
+			resultService.save(result);
+		}catch(Exception e){
+			logger.error("*****订单号码：" + ddhm + "保存结果出现异常，code:"+InvoiceUtil.getIntervalValue(receive.getText(),"<RETURNMSG>","</RETURNMSG>")
+			+ ",msg:" + InvoiceUtil.getIntervalValue(receive.getText(),"<RETURNMSG>","</RETURNMSG>") + ",fpqqlsh:" +
+			InvoiceUtil.getIntervalValue(receive.getText(),"<FPQQLSH>","</FPQQLSH>"));
+		}
 		
 		
         return receive.getText();
--- src/main/java/com/baiwang/einvoice/qz/service/impl/ResultOfKpServiceImpl.java
+++ src/main/java/com/baiwang/einvoice/qz/service/impl/ResultOfKpServiceImpl.java
@@ -20,4 +20,17 @@ public class ResultOfKpServiceImpl implements IResultOfKpService {
 		return dao.queryResult(ddhm);
 	}
 
+	@Override
+	public int save(ResultOfKp result) {
+		
+		int count = dao.selectByDdhm(result.getDdhm());
+		if(count == 0){
+			count = dao.save(result);
+		}else{
+			count = dao.updateByDdhm(result);
+		}
+		
+		return count;
+	}
+
 }
