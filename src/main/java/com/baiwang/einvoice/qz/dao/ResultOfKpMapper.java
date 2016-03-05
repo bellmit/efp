dcb6c4e730a905bf6d0c38f6429b239a9962d60e
gaokemeng <gaokemeng@baiwang.com>
2016-03-05 14:13:12 +0800

    Signed-off-by: gaokemeng <gaokemeng@baiwang.com>

--- src/main/java/com/baiwang/einvoice/qz/dao/ResultOfKpMapper.java
+++ src/main/java/com/baiwang/einvoice/qz/dao/ResultOfKpMapper.java
@@ -2,6 +2,7 @@ package com.baiwang.einvoice.qz.dao;
 
 import org.apache.ibatis.annotations.Param;
 
+import com.baiwang.einvoice.qz.beans.Kpxx;
 import com.baiwang.einvoice.qz.beans.ResultOfKp;
 
 public interface ResultOfKpMapper {
@@ -17,11 +18,16 @@ public interface ResultOfKpMapper {
 
     int updateByPrimaryKey(ResultOfKp record);
 
-	ResultOfKp queryResult(@Param("ddhm")String ddhm, @Param("correlationId")String correlationId);
+	ResultOfKp queryResult2(@Param("ddhm")String ddhm, @Param("correlationId")String correlationId);
 
 	int save(ResultOfKp result);
 
 	int selectByDdhm(@Param("ddhm") String ddhm);
 
 	int updateByDdhm(ResultOfKp result);
+
+	/**
+	 * 进入前置先根据两个订单号、发票类型查询结果
+	 */
+	Kpxx queryResult(@Param("zddh")String zddh, @Param("fddh")String fddh, @Param("fplx")String fplx);
 }
