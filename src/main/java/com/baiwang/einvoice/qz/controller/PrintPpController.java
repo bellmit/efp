8950f896a8c15e4296021037c7de54bd64dc8d52
gaokemeng <gaokemeng@baiwang.com>
2016-03-07 16:57:03 +0800

    Signed-off-by: gaokemeng <gaokemeng@baiwang.com>

--- src/main/java/com/baiwang/einvoice/qz/controller/PrintPpController.java
+++ src/main/java/com/baiwang/einvoice/qz/controller/PrintPpController.java
@@ -4,32 +4,72 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.baiwang.einvoice.qz.beans.User;
 import com.baiwang.einvoice.qz.service.IPrintPpService;
 
 @Controller
-@RequestMapping("printpp")
+@RequestMapping("")
 public class PrintPpController {
+	
+	private static final Log logger = LogFactory.getLog(PrintPpController.class);
 	
 	@Resource
 	private IPrintPpService service;
 
-	@RequestMapping("printlist")
+	//单打
+	@RequestMapping("printpp/printlist")
 	@ResponseBody
-	public List<Map<String,String>> getPrintPpList(String beginDate, String endDate, String kpdq, String zddh){
-		System.out.println(beginDate);
-		System.out.println(endDate);
-		System.out.println(kpdq);
-		System.out.println(zddh);
+	public List<Map<String,String>> getPrintPpList(String beginDate, String endDate, String kpdq, String zddh, String fplx, HttpServletRequest request){
+		User user = (User)request.getSession().getAttribute("user");
+		if(null != user){
+			logger.info("***用户名：" + user.getUserName() + "," + beginDate +","+endDate +"," +kpdq +","+ zddh +","+fplx);
+			/*if(null != kpdq && kpdq.equals(user.getUserType())){
+				
+			}*/
+		}
 		
-		List<Map<String,String>> fapiaoVo = service.getPrintPpList(beginDate, endDate, kpdq, zddh);
+		List<Map<String,String>> fapiaoVo = service.getPrintPpList(beginDate, endDate, kpdq, zddh, fplx);
 		System.out.println(fapiaoVo.size());
 		
 		return fapiaoVo;
 	}
+	
+	//连打
+	@RequestMapping("printpps/printlist")
+	@ResponseBody
+	public List<Map<String,String>> getPrintPpsList(String beginDate, String endDate, String beginfphm, String endfphm, String fplx, HttpServletRequest request){
+		User user = (User)request.getSession().getAttribute("user");
+		if(null != user){
+			logger.info("***用户名：" + user.getUserName() + "," + beginDate +","+endDate +"," +beginfphm +","+ endfphm );
+			/*if(null != kpdq && kpdq.equals(user.getUserType())){
+				
+			}*/
+		}
+		
+		List<Map<String,String>> fapiaoVo = service.getPrintPpsList(beginDate, endDate, beginfphm, endfphm, fplx);
+		System.out.println(fapiaoVo.size());
+		
+		return fapiaoVo;
+	}
+	@RequestMapping("printpps/showDetail")
+	@ResponseBody
+	public List<Map<String,String>> showDetail(String begin, String end, String fplx){
+		
+		System.out.println(begin);
+		System.out.println(end);
+		System.out.println(fplx);
+		
+		return service.showDetail(begin, end, fplx);
+	}
+	
+	
 	
 }
--- src/main/java/com/baiwang/einvoice/qz/service/impl/PrintPpServiceImpl.java
+++ src/main/java/com/baiwang/einvoice/qz/service/impl/PrintPpServiceImpl.java
@@ -1,10 +1,13 @@
 package com.baiwang.einvoice.qz.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
 import com.baiwang.einvoice.qz.dao.PrintDaoMapper;
@@ -17,9 +20,73 @@ public class PrintPpServiceImpl implements IPrintPpService {
 	private PrintDaoMapper dao;
 	
 	@Override
-	public List<Map<String,String>> getPrintPpList(String beginDate, String endDate, String kpdq, String zddh) {
+	public List<Map<String,String>> getPrintPpList(String beginDate, String endDate, String kpdq, String zddh, String fplx) {
 		
-		return dao.getPrintPpList(beginDate, endDate, kpdq, zddh);
+		return dao.getPrintPpList(beginDate, endDate, kpdq, zddh, fplx);
+	}
+
+	@Override
+	public List<Map<String, String>> getPrintPpsList(String beginDate, String endDate, String beginfphm,
+			String endfphm, String fplx) {
+		
+		List<Map<String, String>> _list = new ArrayList<Map<String, String>>();
+		
+		List<Map<String, String>> list = dao.getPrintPpsList(beginDate, endDate, beginfphm, endfphm, fplx);
+		if(null != list && list.size() > 0){
+			
+			String _beginDate = list.get(0).get("kprq");
+			String _endDate = list.get(0).get("kprq");
+			int _beginfphm = Integer.valueOf(list.get(0).get("fphm"));
+			int _endfphm = _beginfphm;
+			System.out.println("第一个发票号码：" + _beginfphm);
+			
+			int _beforefphm = _endfphm;
+			
+			for(int i = 1; i < list.size(); i++){
+				Map<String, String> _map = new HashMap<String, String>();
+				
+				Map<String, String> map = list.get(i);
+				
+				int fphm = Integer.valueOf(map.get("fphm"));
+				System.out.println("第" + i + "个发票号码：" + fphm);
+				
+				
+				if(fphm == (_beforefphm + 1)){
+					_endfphm = fphm;
+					_endDate = list.get(i).get("kprq");
+					
+					if(i == (list.size() - 1)){//是否最后一个
+						_map.put("beginfphm", Integer.toString(_beginfphm));
+						_map.put("endfphm", Integer.toString(_endfphm));
+						_map.put("beginDate", _beginDate);
+						_map.put("endDate", _endDate);
+						_list.add(_map);
+					}
+				}else{
+					_map.put("beginfphm", Integer.toString(_beginfphm));
+					_map.put("endfphm", Integer.toString(_endfphm));
+					_map.put("beginDate", _beginDate);
+					_map.put("endDate", _endDate);
+					_list.add(_map);
+					
+					_beginfphm = fphm;
+//					_endfphm = fphm;
+					_beginDate = list.get(i).get("kprq");
+//					_endDate = list.get(i).get("kprq");
+				}
+				_beforefphm = fphm;
+				
+			}
+		}
+		
+		
+		return _list;
+	}
+
+	@Override
+	public List<Map<String, String>> showDetail(String begin, String end, String fplx) {
+		
+		return dao.showDetail(begin, end, fplx);
 	}
 
 }
