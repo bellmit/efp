082303095dd9b5e52139497f71e11454102e8213
zhaowei <zhaowei@baiwang.com>
2016-03-08 15:36:47 +0800

    增加用户管理模块

--- src/main/java/com/baiwang/einvoice/qz/service/impl/UserServiceImpl.java
+++ src/main/java/com/baiwang/einvoice/qz/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.baiwang.einvoice.qz.service.impl;
 
+import java.util.HashMap;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
@@ -7,12 +9,17 @@ import org.springframework.stereotype.Service;
 import com.baiwang.einvoice.qz.beans.User;
 import com.baiwang.einvoice.qz.dao.UserMapper;
 import com.baiwang.einvoice.qz.service.IUserService;
+import com.baiwang.einvoice.qz.service.PageServiceImpl;
+import com.github.miemiedev.mybatis.paginator.domain.PageList;
 
 @Service
 public class UserServiceImpl implements IUserService {
 	
 	@Resource
 	private UserMapper dao;
+	
+	@Resource
+	private PageServiceImpl pageService;
 
 	public User getUserByName(String name) {
 		
@@ -47,6 +54,49 @@ public class UserServiceImpl implements IUserService {
 		
 		// TODO Auto-generated method stub
 		dao.changePass(id, user_pass);
+	}
+
+	/**
+	  * <p>Title: listUser</p>
+	  * <p>Description: </p>
+	  * @return
+	  * @see com.baiwang.einvoice.qz.service.IUserService#listUser()
+	  */
+	@SuppressWarnings("unchecked")
+	@Override
+	public PageList<HashMap<String, Object>> listUser(HashMap<String, String> param, int pageIndex, int pageSize) {
+		
+		// TODO Auto-generated method stub
+		return (PageList<HashMap<String, Object>>) pageService.getPageList(UserMapper.class, "listUser",param, pageIndex, pageSize);
+		
+	}
+
+	/**
+	  * <p>Title: selectById</p>
+	  * <p>Description: </p>
+	  * @param id
+	  * @return
+	  * @see com.baiwang.einvoice.qz.service.IUserService#selectById(int)
+	  */
+	@Override
+	public User selectById(int id) {
+		
+		// TODO Auto-generated method stub
+		return dao.selectByPrimaryKey(id);
+		
+	}
+
+	/**
+	  * <p>Title: updateById</p>
+	  * <p>Description: </p>
+	  * @param id
+	  * @see com.baiwang.einvoice.qz.service.IUserService#updateById(int)
+	  */
+	@Override
+	public void updateById(User user) {
+		
+		// TODO Auto-generated method stub
+		dao.updateByPrimaryKeySelective(user);
 	}
 
 }
