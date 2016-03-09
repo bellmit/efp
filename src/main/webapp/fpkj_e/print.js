852ffdd320dd5f74bbcecf75dd482c4b54b1b1c8
lidongming <lidongming@baiwang.com>
2016-03-09 16:05:27 +0800

    更新电子发票开具

--- src/main/webapp/fpkj_e/print.js
+++ src/main/webapp/fpkj_e/print.js
@@ -1,25 +1,25 @@
-//����ؼ�
+﻿//输出控件
 document.write("<object id=\"LODOP_OB\" "
 		+ "classid=\"clsid:2105C259-1E0C-4534-8141-A753534CB4CA\" "
 		+ "width=\"0\" height=\"0\">"
 		+ "<embed id=\"LODOP_EM\" type=\"application/x-print-lodop\" "
 		+ "width=0 height=0></embed></object>");
-//��ӡ�ؼ�
+//打印控件
 var	LODOP;
-//�ͻ��������ļ�·��
+//客户机配置文件路径
 var cfg_dir = "C:\\Program Files\\MountTaiSoftware\\Lodop\\";
-//��ȡ�ؼ�ʵ��
+//获取控件实例
 if (navigator.appVersion.indexOf("MSIE")>=0){
 	LODOP=document.getElementById('LODOP_OB');
 }else{
 	LODOP=document.getElementById('LODOP_EM');
 }
 if (LODOP==null||typeof(LODOP.VERSION)=="undefined"||LODOP.VERSION!='6.1.9.8'){
-	location.href="";
+	location.href="../kphy/kjxz.xhtml";
 }else{
-	LODOP.SET_LICENSES("ZCSB�������з�����","545C44BD459DBBAFB79F18A0F8160A12","","");
+	LODOP.SET_LICENSES("ZCSB技术处研发中心","545C44BD459DBBAFB79F18A0F8160A12","","");
 }
-//���ƽ��������
+//限制金额项数字
 function limit_money_input(a) {
 	var ja = $(a);
 	ja.bind("contextmenu", function() {
@@ -50,9 +50,9 @@ function addMoney(a,b){
 	LODOP.ADD_PRINT_TEXT(a-1,b,20,20,"Y");
 	LODOP.SET_PRINT_STYLEA(0,"FontSize",12);
 	LODOP.ADD_PRINT_TEXT(a-9,b,20,20,"_");
-	LODOP.SET_PRINT_STYLEA(0,"FontName","����");
+	LODOP.SET_PRINT_STYLEA(0,"FontName","楷体");
 	LODOP.SET_PRINT_STYLEA(0,"FontSize",12);
 	LODOP.ADD_PRINT_TEXT(a-7,b,20,20,"_");
-	LODOP.SET_PRINT_STYLEA(0,"FontName","����");
+	LODOP.SET_PRINT_STYLEA(0,"FontName","楷体");
 	LODOP.SET_PRINT_STYLEA(0,"FontSize",12);
 }
