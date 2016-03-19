a1350af41723b8153a43af0df8f5b0ef1ed64dee
gaokemeng <gaokemeng@baiwang.com>
2016-03-19 17:22:23 +0800

    Signed-off-by: gaokemeng <gaokemeng@baiwang.com>

--- src/main/webapp/js/downloadocx.js
+++ src/main/webapp/js/downloadocx.js
@@ -2,6 +2,6 @@
 try{
 	var micx=new ActiveXObject('NISECSKSCX.NISECSKSCXCtrl.1');
 }catch(e)  {
-	alert("您未安装打印控件,请先安装...");
+	window.parent.$.messager.alert('消息',"您未安装打印控件,请先安装...");
 	location.href='../download/skfwqzjjk-bzbV1.1.0.9.EXE';
 }
