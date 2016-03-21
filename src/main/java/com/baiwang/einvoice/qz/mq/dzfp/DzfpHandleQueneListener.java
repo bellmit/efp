3ed283904ad03e6fa689e25ba8eaed4a9cd6f924
gaokemeng <gaokemeng@baiwang.com>
2016-03-21 18:49:49 +0800

    Signed-off-by: gaokemeng <gaokemeng@baiwang.com>

--- src/main/java/com/baiwang/einvoice/qz/mq/dzfp/DzfpHandleQueneListener.java
+++ src/main/java/com/baiwang/einvoice/qz/mq/dzfp/DzfpHandleQueneListener.java
@@ -72,7 +72,7 @@ public class DzfpHandleQueneListener implements SessionAwareMessageListener {
 				List<Fpmx> list = business.getREQUESTCOMMONFPKJ().getCommonfpkjxmxxs().getFpmx();
 				System.out.println("发票请求流水号："+ fpqqlsh);
 				
-				Map<String, String> result = resultService.queryResult(orderDetail.getZddh(), orderDetail.getFddh(), kpxx.getFplx());//根据两个订单号查
+				Map<String, String> result = resultService.queryResult(fpqqlsh, kpxx.getFplx());//根据两个订单号查
 				
 				if(null == result || result.get("returnCode").equals("4000")){
 					fpService.saveInfo(orderDetails, kpxx, list , fpqqlsh);
@@ -137,12 +137,12 @@ public class DzfpHandleQueneListener implements SessionAwareMessageListener {
 					resultService.saveResultOfSk(resultMap);
 					
 				}else if("0000".equals(result.get("returnCode"))){
-					logger.warn("*********订单号：" + orderDetail.getZddh()+"/"+orderDetail.getFddh() + "已经开票成功，返回。");
+					logger.warn("*********发票请求流水号：" +fpqqlsh + "已经开票成功，返回。");
 					textMessage = session.createTextMessage(InvoiceUtil.backMsg("0000", "发票已开具成功", xml));
 					textMessage.setJMSCorrelationID(message.getJMSCorrelationID());
 					jmsTemplate2.convertAndSend(textMessage);
 				}else{
-					logger.warn("*********订单号：" + orderDetail.getZddh()+"/"+orderDetail.getFddh() + ",returnCode:" + 
+					logger.warn("*********发票请求流水号：" +fpqqlsh + ",returnCode:" + 
 							result.get("returnCode") + ",returnMsg:" + result.get("returnMsg"));
 					textMessage = session.createTextMessage(InvoiceUtil.backMsg(result.get("returnCode"), result.get("returnMsg"), xml));
 					textMessage.setJMSCorrelationID(message.getJMSCorrelationID());
--- src/main/java/com/baiwang/einvoice/qz/service/impl/ResultOfSkServiceImpl.java
+++ src/main/java/com/baiwang/einvoice/qz/service/impl/ResultOfSkServiceImpl.java
@@ -33,49 +33,49 @@ public class ResultOfSkServiceImpl implements IResultOfSkService {
 	/**
 	 * 进入前置先根据两个订单号、发票类型查询结果
 	 */
-	public Map<String, String> queryResult(String zddh, String fddh, String fplx) {
-		logger.info("** 订单查询   **主订单号：" + zddh +",副订单号：" + fddh + ",发票类型：" + fplx);
+	public Map<String, String> queryResult(String fpqqlsh, String fplx) {
+		logger.info("** 订单查询   **发票请求流水号：" + fpqqlsh + ",发票类型：" + fplx);
 		Map<String, String> map = new HashMap<String, String>();
-		Kpxx kpxx = resultDao.queryResult(zddh, fddh, fplx);
+		Kpxx kpxx = resultDao.queryResult( fpqqlsh, fplx);
 		
 		if(null == kpxx){
-			logger.info("** 订单查询   **主订单号：" + zddh +",副订单号：" + fddh + ",发票类型：" + fplx + "查询为null");
+			logger.info("** 订单查询   **发票请求流水号：" + fpqqlsh + ",发票类型：" + fplx + "查询为null");
 			return null;
 		}else if("026".equals(kpxx.getFplx())){//电子票
 			if("1".equals(kpxx.getFpzt())){
-				logger.info("** 订单查询   **主订单号：" + zddh +",副订单号：" + fddh + ",发票类型：" + fplx + "查询为fpzt:1");
+				logger.info("** 订单查询    **发票请求流水号：" + fpqqlsh + ",发票类型：" + fplx + "查询为fpzt:1");
 				map.put("returnCode", "2000");
 				map.put("returnMsg", "电子开票正在处理中");
 				return map;
 			}else if("-1".equals(kpxx.getFpzt())){
-				logger.info("** 订单查询   **主订单号：" + zddh +",副订单号：" + fddh + ",发票类型：" + fplx + ",开具成功，查询为fpzt:" + kpxx.getFpzt());
+				logger.info("** 订单查询    **发票请求流水号：" + fpqqlsh + ",发票类型：" + fplx + ",开具成功，查询为fpzt:" + kpxx.getFpzt());
 				map.put("returnCode", "4000");
 				map.put("returnMsg", kpxx.getResultmsg());
 				return map;
 			}else{
-				logger.info("** 订单查询   **主订单号：" + zddh +",副订单号：" + fddh + ",发票类型：" + fplx + ",开具成功，查询为fpzt:" + kpxx.getFpzt());
+				logger.info("** 订单查询    **发票请求流水号：" + fpqqlsh +  ",发票类型：" + fplx + ",开具成功，查询为fpzt:" + kpxx.getFpzt());
 				map.put("returnCode", "0000");
 				map.put("returnMsg", "电子开票成功");
 				return map;
 			}
 		}else{//纸质发票
 			if("1".equals(kpxx.getFpzt())){
-				logger.info("** 订单查询   **主订单号：" + zddh +",副订单号：" + fddh + ",发票类型：" + fplx + "查询为fpzt:1");
+				logger.info("** 订单查询     **发票请求流水号：" + fpqqlsh +  ",发票类型：" + fplx + "查询为fpzt:1");
 				map.put("returnCode", "2000");
 				map.put("returnMsg", "待开票");
 				return map;
 			}else if("2".equals(kpxx.getFpzt())){
-				logger.info("** 订单查询   **主订单号：" + zddh +",副订单号：" + fddh + ",发票类型：" + fplx + "查询为fpzt:2");
+				logger.info("** 订单查询     **发票请求流水号：" + fpqqlsh +  ",发票类型：" + fplx + "查询为fpzt:2");
 				map.put("returnCode", "2000");
 				map.put("returnMsg", "待打印");
 				return map;
 			}else if("-1".equals(kpxx.getFpzt())){
-				logger.info("** 订单查询   **主订单号：" + zddh +",副订单号：" + fddh + ",发票类型：" + fplx + ",开具成功，查询为fpzt:" + kpxx.getFpzt());
+				logger.info("** 订单查询    **发票请求流水号：" + fpqqlsh + ",发票类型：" + fplx + ",开具成功，查询为fpzt:" + kpxx.getFpzt());
 				map.put("returnCode", "4000");
 				map.put("returnMsg", kpxx.getResultmsg());
 				return map;
 			}else{
-				logger.info("** 订单查询   **主订单号：" + zddh +",副订单号：" + fddh + ",发票类型：" + fplx + ",开具成功，查询为fpzt:" + kpxx.getFpzt());
+				logger.info("** 订单查询    **发票请求流水号：" + fpqqlsh + ",发票类型：" + fplx + ",开具成功，查询为fpzt:" + kpxx.getFpzt());
 				map.put("returnCode", "0000");
 				map.put("returnMsg", "开票成功");
 				return map;
