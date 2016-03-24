d290642dbcab07386d87cab7441cdbf3e6fa74ff
zhaowei <zhaowei@baiwang.com>
2016-03-24 18:44:49 +0800

    修改条件选择问题

--- src/main/java/com/baiwang/einvoice/qz/utils/ValidateXML.java
+++ src/main/java/com/baiwang/einvoice/qz/utils/ValidateXML.java
@@ -50,25 +50,24 @@ public class ValidateXML {
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		String xml = "<?xml version=\"1.0\" encoding=\"gbk\"?> <business id=\"FPKJ\"> 	\r\n" + 
 				"<CUSTOMDATA>\r\n" + 
-				"<DDXXS LSH=\"11112222333344445555\">\r\n" + 
+				"<DDXXS LSH=\"66238462181003115098\">\r\n" + 
 				"<DDXX>\r\n" + 
-				"<ZDDH>100008</ZDDH> \r\n" + 
-				"<SQR>张三</SQR>\r\n" + 
-				"<HYM>会员名</HYM>\r\n" + 
+				"<ZDDH>100011</ZDDH> \r\n" + 
 				"<HYID>会员id</HYID>\r\n" + 
-				"<DDSJ>2016-03-22 10:29:32</DDSJ>\r\n" + 
-				"<SQSJ>2016-03-22 10:29:33</SQSJ>\r\n" + 
-				"<FPTT>个人</FPTT>\r\n" + 
-				"<FPKH>B2B/B2C</FPKH>\r\n" + 
-				"<SPZL>服务费</SPZL>\r\n" + 
-				"<SQRK>erp</SQRK>\r\n" + 
-				"<FPDQ>发票地区</FPDQ>\r\n" + 
+				"<DDSJ>2016-03-22 17:23:57</DDSJ>\r\n" + 
+				"<SQSJ>2016-03-22 11:42:00</SQSJ>\r\n" + 
+				"<FKSJ>2016-03-22 17:23:57</FKSJ>\r\n" + 
+				"<FPTTLX>00</FPTTLX>\r\n" + 
+				"<FPTT>公司的专票</FPTT>\r\n" + 
+				"<FPKHLX>00</FPKHLX>\r\n" + 
+				"<SQRK>00</SQRK>\r\n" + 
+				"<FPDQ>00</FPDQ>\r\n" + 
 				"<SHR>收货人</SHR>\r\n" + 
 				"<SHRDH>收货人电话</SHRDH>\r\n" + 
 				"<JSDZ>寄送地址</JSDZ>\r\n" + 
-				"<TKZT>退款状态</TKZT>\r\n" + 
-				"<FPCLZT>发票处理状态</FPCLZT>\r\n" + 
 				"<BZFP>备注发票信息</BZFP>\r\n" + 
+				"<SJHM></SJHM>\r\n" + 
+				"<YX></YX>\r\n" + 
 				"</DDXX>\r\n" + 
 				"</DDXXS>\r\n" + 
 				"</CUSTOMDATA>\r\n" + 
@@ -81,10 +80,10 @@ public class ValidateXML {
 				"<XSF_DZ>销售方地址</XSF_DZ> \r\n" + 
 				"<XSF_DH>11012345678</XSF_DH>\r\n" + 
 				"<XSF_YHZH>622848265825566332</XSF_YHZH>\r\n" + 
-				"<GMF_NSRSBH></GMF_NSRSBH> \r\n" + 
+				"<GMF_NSRSBH>11010800000000000007</GMF_NSRSBH> \r\n" + 
 				"<GMF_MC>个人</GMF_MC> 	\r\n" + 
-				"<GMF_DZ></GMF_DZ> 	\r\n" + 
-				"<GMF_DH></GMF_DH> 			\r\n" + 
+				"<GMF_DZ>北京海淀</GMF_DZ> 	\r\n" + 
+				"<GMF_DH>12345678</GMF_DH> 			\r\n" + 
 				"<GMF_YHZH>888888888888888</GMF_YHZH> 	\r\n" + 
 				"<KPR>bw0701</KPR> 		\r\n" + 
 				"<SKR>bw0701</SKR> 			\r\n" + 
@@ -92,8 +91,8 @@ public class ValidateXML {
 				"<YFP_DM></YFP_DM> 		\r\n" + 
 				"<YFP_HM></YFP_HM> 	\r\n" + 
 				"<JSHJ>58.50</JSHJ> 		\r\n" + 
-				"<HJJE>50.00</HJJE> 		\r\n" + 
-				"<HJSE>8.50</HJSE> 		\r\n" + 
+				"<HJJE></HJJE> 		\r\n" + 
+				"<HJSE></HJSE> 		\r\n" + 
 				"<BZ>备注</BZ> 	\r\n" + 
 				"</COMMON_FPKJ_FPT>\r\n" + 
 				"<COMMON_FPKJ_XMXXS> \r\n" + 
@@ -103,24 +102,12 @@ public class ValidateXML {
 				"<DW></DW> 		\r\n" + 
 				"<GGXH></GGXH> 		\r\n" + 
 				"<XMSL>1</XMSL> 		\r\n" + 
-				"<XMDJ>10.00</XMDJ> 			\r\n" + 
-				"<XMJE>10.00</XMJE> 			\r\n" + 
-				"<SL>0.17</SL> 			\r\n" + 
-				"<SE>1.7</SE> 			\r\n" + 
+				"<XMDJ></XMDJ> 			\r\n" + 
+				"<XMJE></XMJE> 			\r\n" + 
+				"<SL>0.06</SL> 			\r\n" + 
+				"<SE></SE> 			\r\n" + 
 				"<HSBZ>0</HSBZ> 		\r\n" + 
 				"</COMMON_FPKJ_XMXX> 	\r\n" + 
-				"<COMMON_FPKJ_XMXX> 		\r\n" + 
-				"<FPHXZ>0</FPHXZ> 		\r\n" + 
-				"<XMMC>华潮 E386</XMMC> 	\r\n" + 
-				"<DW></DW> 			\r\n" + 
-				"<GGXH></GGXH> 			\r\n" + 
-				"<XMSL>2</XMSL> 		\r\n" + 
-				"<XMDJ>20.00</XMDJ> 			\r\n" + 
-				"<XMJE>40.00</XMJE> 			\r\n" + 
-				"<SL>0.17</SL> 			\r\n" + 
-				"<SE>6.80</SE> 			\r\n" + 
-				"<HSBZ>0</HSBZ> 		\r\n" + 
-				"</COMMON_FPKJ_XMXX> \r\n" + 
 				"</COMMON_FPKJ_XMXXS> \r\n" + 
 				"</REQUEST_COMMON_FPKJ>\r\n" + 
 				"</business>";
